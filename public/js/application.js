e0c0ae40c630dfedc591b24ec3dc99222e712cb8
digbyk <digby@digby.net>
2014-09-16 22:46:51 +0100

    More angular

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,9 @@ var aiwfApp = angular.module('AiwfApp', []);
 
 aiwfApp.controller('GiftController', function ($scope, $http) {
 	$http.get('/api/lists').success(function (data) {
+		$scope.lists = data;
+	});
+	$http.get('/api/gifts').success(function (data) {
 		$scope.gifts = data;
 	});
 });
